eb00c6d8556f8691c6060ec2b18d9e3833400eee
wyash12 <wyash2752@gmail.com>
2021-11-08 20:16:10 +0530

    added operator buttons

--- project_caclcu.py
+++ project_caclcu.py
@@ -18,8 +18,20 @@ class Calculator:
             0: (4, 2), '.': (4, 1)
         }
         self.total_label, self.label = self.create_display_label()
+        self.operations = {"/": "\u00F7", "*": "\u00D7", "-": "-", "+": "+"}
         self.button_frame = self.create_buttons_frame()
+
+        self.button_frame.rowconfigure(0, weight=1)
+        for i in range(1,5):
+            self.button_frame.rowconfigure(i, weight=1)
+            self.button_frame.columnconfigure(i, weight=1)
         self.create_digit_buttons()
+        self.create_operator_buttons()
+        self.create_special_buttons()
+
+    def create_special_buttons(self):
+        self.create_clear_button()
+        self.create_equals_button()
 
     def create_display_label(self):
         total_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, bg="#F5F5F5",
@@ -40,6 +52,28 @@ class Calculator:
             buttons = tk.Button(self.button_frame, text=str(digit), bg="#FFFFFF",fg="#25265E",
                                font=("Arial", 24, "bold"), borderwidth=0)
             buttons.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
+    def create_operator_buttons(self):
+        i=0
+        for operator,symbol in self.operations.items():
+            buttons=tk.Button(self.button_frame, text=symbol,bg="#F8FAFF" ,fg="#25265E",font=("Arial", 20),borderwidth=0)
+            buttons.grid(row=i,column=4,sticky=tk.NSEW)
+            i+=1
+
+    # def clear(self):
+    #     self.current_expression = ""
+    #     self.total_expression = ""
+    #     self.update_label()
+    #     self.update_total_label()
+
+    def create_clear_button(self):
+        button = tk.Button(self.button_frame, text="C", bg="#F8FAFF" ,fg="#25265E",font=("Arial", 20),
+                           borderwidth=0)
+        button.grid(row=0, column=1,columnspan=3, sticky=tk.NSEW)
+
+    def create_equals_button(self):
+        button = tk.Button(self.button_frame, text="=", bg="#CCEDFF" ,fg="#25265E",font=("Arial", 20),
+                           borderwidth=0)
+        button.grid(row=4, column=3,columnspan=2, sticky=tk.NSEW)
 
     def create_buttons_frame(self):
         frame = tk.Frame(self.root)
